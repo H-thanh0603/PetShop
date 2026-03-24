77b834d0b0d24352a38e6b7122ff744c6e5eaa5d
phamdat1234 <23130056@st.hcmuaf.edu.vn>
2026-03-24 19:29:39 +0700

    fix thêm danh sách địa chỉ trong trang thanh toán

--- src/main/java/DAO/AddressDao.java
+++ src/main/java/DAO/AddressDao.java
@@ -1,4 +1,109 @@
 package DAO;
 
+import Context.DBContext;
+import Model.Address;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddressDao {
+    public void insertAddress(int userId, String address, boolean defaultt, Timestamp created_at) {
+        String sql = "INSERT INTO user_addresses(user_id, address, defaultt, created_at) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection con = DBContext.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, userId);
+            ps.setString(2, address);
+            ps.setBoolean(3, defaultt);
+            ps.setTimestamp(4, created_at);
+
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void setDefaultAddress(int userId, int addressId) {
+        String resetSql = "UPDATE addresses SET defaultt = 0 WHERE user_id = ?";
+        String setDefaultSql = "UPDATE addresses SET defaultt = 1 WHERE id = ? AND user_id = ?";
+        try (Connection conn = DBContext.getConnection();) {
+            conn.setAutoCommit(false);
+            try (PreparedStatement psReset = conn.prepareStatement(resetSql)) {
+                psReset.setInt(1, userId);
+                psReset.executeUpdate();
+            }
+            try (PreparedStatement psSet = conn.prepareStatement(setDefaultSql)) {
+                psSet.setInt(1, addressId);
+                psSet.setInt(2, userId);
+                psSet.executeUpdate();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Address> getAddressesByUser(int userId) {
+        List<Address> list = new ArrayList<>();
+        String sql = "SELECT * FROM addresses WHERE user_id = ?";
+
+        try (Connection con = DBContext.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setInt(1, userId);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Address addr = new Address();
+                addr.setId(rs.getInt("id"));
+                addr.setUserId(rs.getInt("user_id"));
+                addr.setDefaultt(rs.getBoolean("defaultt"));
+                addr.setCreateAt(rs.getTimestamp("created_at"));
+                addr.setAddress(rs.getString("address"));
+                addr.setProvince(rs.getString("province"));
+                addr.setDistrict(rs.getString("district"));
+                addr.setWard(rs.getString("ward"));
+                list.add(addr);
+                ps.executeUpdate();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+    public void addAddress(int userId, boolean defaultt, Timestamp created_at,  String address, String province, String district, String ward) {
+        String insertSql = "INSERT INTO addresses (user_id, defaultt, created_at, address, province, district, ward) VALUES (?,?,?,?,?,?,?)";
+        String resetSql = "UPDATE addresses SET defaultt = 0 WHERE user_id = ?";
+        try (Connection conn = DBContext.getConnection();) {
+            conn.setAutoCommit(false);
+
+            if (defaultt) {
+                try (PreparedStatement psReset = conn.prepareStatement(resetSql)) {
+                    psReset.setInt(1, userId);
+                    psReset.executeUpdate();
+                }
+            }
+
+            try (PreparedStatement ps = conn.prepareStatement(insertSql)) {
+                ps.setInt(1, userId);
+                ps.setBoolean(2, defaultt);
+                ps.setTimestamp(3, created_at);
+                ps.setString(4, address);
+                ps.setString(5, province);
+                ps.setString(6, district);
+                ps.setString(7, ward);
+                ps.executeUpdate();
+            }
+
+            conn.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/controller/shop/CheckoutServlet.java
+++ src/main/java/controller/shop/CheckoutServlet.java
@@ -1,6 +1,10 @@
 package controller.shop;
 
 import java.io.IOException;
+
+import DAO.AddressDao;
+import DAO.UserDAO;
+import Model.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,10 +12,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-import Model.Order;
-import Model.OrderItem;
-import Model.CartItem;
-import Model.User;
 import DAO.OrderDAO;
 
 import java.util.ArrayList;
@@ -21,18 +21,50 @@ import java.util.Map;
 @WebServlet("/checkout")
 public class CheckoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
 
+        HttpSession session = request.getSession();
+        User userSession = (User) session.getAttribute("user");
+
+        if (userSession == null) {
+            response.sendRedirect(request.getContextPath() + "/login");
+            return;
+        }
+        UserDAO userdao = new UserDAO();
+        User user = userdao.getUserById(userSession.getId());
+        session.setAttribute("user", user);
+        Map<Integer, CartItem> cart = (Map<Integer, CartItem>) session.getAttribute("cart");
+        if (cart == null || cart.isEmpty()) {
+            response.sendRedirect(request.getContextPath() + "/shop");
+            return;
+        }
+        AddressDao addressDAO = new AddressDao();
+        List<Address> addressList = addressDAO.getAddressesByUser(user.getId());
+        List<CartItem> cartItems = new ArrayList<>(cart.values());
+        double totalAmount = 0;
+        for (CartItem item : cart.values()) {
+            totalAmount += item.getTotalPrice();
+        }
+        request.setAttribute("addressList", addressList);
+        request.setAttribute("totalAmount", totalAmount);
+        request.setAttribute("cartItems", cartItems);
+        request.setAttribute("user", user);
+        request.getRequestDispatcher("/pages/shop/checkout.jsp").forward(request, response);
+    }
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
         request.setCharacterEncoding("UTF-8");
         HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
-        
-        if (user == null) {
+        User userSession = (User) session.getAttribute("user");
+
+        if (userSession == null) {
             response.sendRedirect(request.getContextPath() + "/login");
             return;
         }
+        UserDAO userdao = new UserDAO();
+        User user = userdao.getUserById(userSession.getId());
 
         Map<Integer, CartItem> cart = (Map<Integer, CartItem>) session.getAttribute("cart");
         if (cart == null || cart.isEmpty()) {
@@ -44,57 +76,52 @@ public class CheckoutServlet extends HttpServlet {
         for (CartItem item : cart.values()) {
             totalAmount += item.getTotalPrice();
         }
-        request.setAttribute("totalAmount", totalAmount);
-        request.setAttribute("cartItems", cartItems);
-        request.setAttribute("user", user);
-        request.getRequestDispatcher("/pages/shop/checkout.jsp").forward(request, response);
-//        // 1. Lấy thông tin từ form
-//        String fullname = user.getFullname();
-//        String phone = user.getPhone();
-//        String email = user.getEmail();
-//        String address = user.getAddress();
-//        String note = request.getParameter("note");
-//        double totalAmount = Double.parseDouble(request.getParameter("totalAmount"));
+
+        // 1. Lấy thông tin từ form
+        String fullname = user.getFullname();
+        String phone = user.getPhone();
+        String address = user.getAddress();
+        String note = request.getParameter("note");
+
+        // 2. Lưu Order
+        Order order = new Order();
+        order.setUserId(user.getId());
+        order.setFullname(fullname);
+        order.setPhone(phone);
+        order.setAddress(address);
+        order.setNote(note);
+        order.setTotalAmount(totalAmount);
+        order.setStatus("Pending");
+
+
+        //lấy ra danh sách các sản phẩm đã tới phần thanh toán
+        OrderDAO orderDAO = new OrderDAO();
+        int orderId = orderDAO.saveOrder(order);
+
+        if (orderId > 0) {
+            // 3. Lưu Order Items
+            for (CartItem ci : cart.values()) {
+                OrderItem oi = new OrderItem();
+                oi.setOrderId(orderId);
+                oi.setProductId(ci.getProduct().getId());
+                oi.setQuantity(ci.getQuantity());
+                oi.setPrice(ci.getProduct().getPrice());
+                orderDAO.saveOrderItem(oi);
+            }
+
+//             4. Xử lý Session
+            session.removeAttribute("cart");
+            session.removeAttribute("totalQuantity");
 //
-//        // 2. Lưu Order
-//        Order order = new Order();
-//        order.setUserId(user.getId());
-//        order.setFullname(fullname);
-//        order.setPhone(phone);
-//        order.setAddress(address);
-//        order.setNote(note);
-//        order.setTotalAmount(totalAmount);
-//        order.setStatus("Pending");
-//
-//        //lấy ra danh sách các sản phẩm đã tới phần thanh toán
-//        OrderDAO orderDAO = new OrderDAO();
-//        int orderId = orderDAO.saveOrder(order);
-//
-//        if (orderId > 0) {
-//            // 3. Lưu Order Items
-//            for (CartItem ci : cart.values()) {
-//                OrderItem oi = new OrderItem();
-//                oi.setOrderId(orderId);
-//                oi.setProductId(ci.getProduct().getId());
-//                oi.setQuantity(ci.getQuantity());
-//                oi.setPrice(ci.getProduct().getPrice());
-//                orderDAO.saveOrderItem(oi);
-//            }
-//
-////             4. Xử lý Session
-//            session.removeAttribute("cart");
-//            session.removeAttribute("totalQuantity");
-////
-////            // 5. Gửi thông báo thành công
-////            session.setAttribute("toastMessage", "🎉 Đặt hàng thành công! Cảm ơn bạn đã ủng hộ.");
-//            session.setAttribute("toastType", "success");
-//
+//            // 5. Gửi thông báo thành công
+            session.setAttribute("toastMessage", "🎉 Đặt hàng thành công! Cảm ơn bạn đã ủng hộ.");
+            session.setAttribute("toastType", "success");
 //            // 6. Chuyển hướng về trang lịch sử đơn hàng
-//            response.sendRedirect(request.getContextPath() + "/my-orders");
-//        } else {
-//            session.setAttribute("toastMessage", "❌ Đã có lỗi xảy ra. Vui lòng thử lại.");
-//            session.setAttribute("toastType", "error");
-//            response.sendRedirect(request.getContextPath() + "/pages/shop/cart.jsp");
-//        }
+            response.sendRedirect(request.getContextPath() + "/my-orders");
+        } else {
+            session.setAttribute("toastMessage", "❌ Đã có lỗi xảy ra. Vui lòng thử lại.");
+            session.setAttribute("toastType", "error");
+                response.sendRedirect(request.getContextPath() + "/cart");
+        }
     }
 }
