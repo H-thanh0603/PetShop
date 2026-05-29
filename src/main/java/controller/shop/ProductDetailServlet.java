cb72061d2e0fecb56d7b869ecbbea93573d2b62b
H-thanh0603 <23130303@st.hcmuaf.edu.vn>
2026-05-30 00:06:29 +0700

    fix: improve star rating CSS + add lengthReviews attribute

--- src/main/java/controller/shop/ProductDetailServlet.java
+++ src/main/java/controller/shop/ProductDetailServlet.java
@@ -45,6 +45,7 @@ public class ProductDetailServlet extends HttpServlet {
 
             ReviewDAO rDao = new ReviewDAO();
             List<Review> listReviews = rDao.getReviewsByProductId(id);
+            int lengthReviews = listReviews.size();
 
             User user = (User) request.getSession().getAttribute("user");
             boolean hasReviewed = user != null && rDao.hasUserReviewedProduct(user.getId(), id);
@@ -58,6 +59,7 @@ public class ProductDetailServlet extends HttpServlet {
             
             request.setAttribute("detail", p);
             request.setAttribute("listReviews", listReviews);
+            request.setAttribute("lengthReviews", lengthReviews);
             request.setAttribute("hasReviewed", hasReviewed);
             request.setAttribute("hasPurchased", hasPurchased);
             request.setAttribute("wishlistProductIds", wishlistIds);
