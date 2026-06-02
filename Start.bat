d213839753420edd9e1bf4c57902442aa8cc15ef
H-thanh0603 <23130303@st.hcmuaf.edu.vn>
2026-06-02 18:43:17 +0700

    Fix start script and bank webhook parsing

--- Start.bat
+++ Start.bat
@@ -11,7 +11,7 @@ set "JAVA_HOME=C:\Program Files\Java\jdk-21"
 if defined PETSHOP_TOMCAT_HOME (
     set "CATALINA_HOME=%PETSHOP_TOMCAT_HOME%"
 ) else (
-    set "CATALINA_HOME=D:\apache-tomcat-10.1.49"
+    set "CATALINA_HOME=E:\apache-tomcat-10.1.49-windows-x64\apache-tomcat-10.1.49"
 )
 
 set "PROJECT_ROOT=%~dp0"
